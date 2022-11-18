3f018437927f80c246fae3e6c6fb897fb49695cd
jsvirani <jaydeepvirani677@gmail.com>
2022-11-18 11:31:20 +0530

    git init

--- app/src/main/java/com/example/optionmenu/MainActivity.java
+++ app/src/main/java/com/example/optionmenu/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.optionmenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item4:
+                Toast.makeText(this, "Item 4 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item5:
+                Toast.makeText(this, "Item 5 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item6:
+                Toast.makeText(this, "Item 6 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item7:
+                Toast.makeText(this, "Video Call", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item8:
+                Toast.makeText(this, "Audio Call", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
